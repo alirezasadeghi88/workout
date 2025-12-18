927e64fd0f28b273360a99e100d469e085aa8324
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 10:12:35 -0800

    add ConfigReader

--- src/homework2/ConfigReader.java
+++ src/homework2/ConfigReader.java
@@ -1,23 +1,24 @@
-package homework;
+package homework2;
 
 import java.io.FileInputStream;
 import java.util.Properties;
 
 public class ConfigReader {
     public static void main(String[] args) {
+
         Properties properties = new Properties();
 
         try {
-                FileInputStream fileInputStream = new FileInputStream("E:\\projects\\workout\\src\\homework\\db.properties");
+            FileInputStream fileInputStream = new FileInputStream("E:\\projects\\workout\\src\\homework\\db.properties");
             properties.load(fileInputStream);
 
             String dbUrl = properties.getProperty("db.url");
             String dbUser = properties.getProperty("db.user");
-            String dbPassword = properties.getProperty("db.password");
+            String dbpassword = properties.getProperty("db.password");
 
-            System.out.println("DataBase URL :" + dbUrl);
-            System.out.println("User :" + dbUser);
-            System.out.println("Password :" + dbPassword);
+            System.out.println("URL:" + dbUrl);
+            System.out.println("USER:" + dbUser);
+            System.out.println("PASSWORD:" + dbpassword);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
