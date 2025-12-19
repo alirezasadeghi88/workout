cb8471102ff9911d96d29aff8ff49d100f3abe1e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 22:55:16 -0800

    add PhoneBook

--- src/homework/PhoneBook.java
+++ src/homework/PhoneBook.java
@@ -1,14 +1,35 @@
-//package homework;
-//
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.util.Scanner;
-//
-//public class PhoneBook {
-//    public static void main(String[] args)throws IOException {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        FileWriter fileWriter = new FileWriter()
-//    }
-//}
+package homework;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PhoneBook {
+    public static void main(String[] args)throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        FileWriter fileWriter = new FileWriter("contacts.txt",true);
+
+        System.out.print("Enter name :");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter phone :");
+        String phone = scanner.nextLine();
+
+        fileWriter.write(name + "," + phone + "\n");
+        fileWriter.close();
+
+        System.out.println("Contacts saved!");
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("contacts.txt"));
+        String line;
+        System.out.println("All contacts :");
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        bufferedReader.close();
+    }
+}
