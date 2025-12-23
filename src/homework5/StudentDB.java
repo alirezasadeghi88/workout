9bf8c57d887d9027c9458f9fbeae7f053234cb55
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-23 11:42:37 -0800

    add StudentDB

---
+++ src/homework5/StudentDB.java
@@ -0,0 +1,12 @@
+package homework5;
+
+public class StudentDB {
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/testdb";
+        String user = "postgres";
+        String password = "postgres";
+
+
+    }
+}
