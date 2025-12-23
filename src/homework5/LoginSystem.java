cc18ddd474f95e9ead8ca4ff7a952c833772ae9c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-23 10:38:10 -0800

    add LoginSystem

---
+++ src/homework5/LoginSystem.java
@@ -0,0 +1,15 @@
+package homework5;
+
+import java.util.Scanner;
+
+public class LoginSystem {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String url = "jdbc:postgresql://localhost:5432/testdb";
+        String user = "postgres";
+        String password = "postgres";
+
+    }
+}
