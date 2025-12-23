bd0ed4ea55bbeb04937800d89dcfb8106e65ea0a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-23 11:15:57 -0800

    add LoginSystem

--- src/homework5/LoginSystem.java
+++ src/homework5/LoginSystem.java
@@ -1,5 +1,7 @@
 package homework5;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Scanner;
 
 public class LoginSystem {
@@ -10,6 +12,17 @@ public class LoginSystem {
         String url = "jdbc:postgresql://localhost:5432/testdb";
         String user = "postgres";
         String password = "postgres";
+
+        try(Connection connection = DriverManager.getConnection(url,user,password)) {
+
+            System.out.println("USER NAME :");
+            String username = scanner.nextLine();
+
+            System.out.println("PASSWORD :");
+            String pwd = scanner.nextLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
